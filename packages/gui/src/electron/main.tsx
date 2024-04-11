1b2b4fb16bb1a5d2450bf532d84eded9be40f6d3
RightSexyOrc <rightsexyorc@protonmail.com>
2024-04-11 14:10:07 +0000

    update Discord link

--- packages/gui/src/electron/main.tsx
+++ packages/gui/src/electron/main.tsx
@@ -775,7 +775,7 @@ function getMenuTemplate() {
         {
           label: i18n._(/* i18n */ { id: 'Chat on Discord' }),
           click: () => {
-            openExternal('https://discord.gg/Cz3gpRpV');
+            openExternal('https://discord.gg/tbcFbmGaG3');
           },
         },
         {
